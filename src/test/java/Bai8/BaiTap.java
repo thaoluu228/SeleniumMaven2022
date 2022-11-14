72e0622b5f7dd086bc7cc4dcdbce4d39a477e0a8
thaolp <thaoluu.310@gmail.com>
2022-11-14 20:58:56 +0700

    12345

--- src/test/java/Bai8/BaiTap.java
+++ src/test/java/Bai8/BaiTap.java
@@ -15,8 +15,6 @@ public class BaiTap extends BaseTest {
         findElement("//input[@id='password']").sendKeys("riseDemo");
         findElement("//button[normalize-space()='Sign in']").click();
     }
-//testing new branch
-    //update branch 123
     public static void enterAddClient(String clientName) {
         findElement("//input[@id='company_name']").sendKeys(clientName);
         findElement("//div[@id='s2id_created_by']").click();
